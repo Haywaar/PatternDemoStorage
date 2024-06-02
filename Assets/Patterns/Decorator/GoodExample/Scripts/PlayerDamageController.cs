5ff222d9b037e65b783d76212aa1c850702d56c8
sa.kazantsev <sa.kazantsev@corp.mail.ru>
2024-06-02 18:39:14 +0200

    refactoring

--- Assets/Patterns/Decorator/GoodExample/Scripts/PlayerDamageController.cs
+++ Assets/Patterns/Decorator/GoodExample/Scripts/PlayerDamageController.cs
@@ -4,26 +4,24 @@ public class PlayerDamageController : MonoBehaviour
 {
     [SerializeField] private float _startDamage;
     private DamageComponent _damageComponent;
-    private DamageDecorator _damageDecorator;
 
     private void Awake()
     {
         _damageComponent = new DefaultDamageComponent(_startDamage);
-        _damageDecorator = new DamageDecorator(_damageComponent);
     }
 
     public float GetDamageValue()
     {
-        return _damageDecorator.GetDamage();
+        return _damageComponent.GetDamage();
     }
 
     public void AddFixedDamage(float damage)
     {
-        _damageDecorator = new AddFixedDamageDecorator(_damageDecorator, damage);
+        _damageComponent = new AddFixedDamageDecorator(_damageComponent, damage);
     }
 
     public void AddPercentBonusDamage(float addPercent)
     {
-        _damageDecorator = new AddPercentageDamageDecorator(_damageDecorator, addPercent);
+        _damageComponent = new AddPercentageDamageDecorator(_damageComponent, addPercent);
     }
 }
